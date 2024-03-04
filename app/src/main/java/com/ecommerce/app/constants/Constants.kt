e9303b66eddafcef2b79c7af404aace9ddfb4748
naveenkm121 <naveeniitrpr100@gmail.com>
2024-03-04 19:56:58 +0530

    chnages in Certificate

--- app/src/main/java/com/ecommerce/app/constants/Constants.kt
+++ app/src/main/java/com/ecommerce/app/constants/Constants.kt
@@ -3,7 +3,9 @@ package com.ecommerce.app.constants
 class Constants {
     companion object {
        // const val BASE_URL = "https://pokeapi.co/api/v2/"
-        const val BASE_URL = "http://ecommerce-rest-apis.ap-southeast-1.elasticbeanstalk.com/v1/"
+       // const val BASE_URL = "http://ecommerce-rest-apis.ap-southeast-1.elasticbeanstalk.com/v1/"
+       //const val BASE_URL = "http://192.168.29.85:8080/ECommerceServicesAuth/v1/"
+        const val BASE_URL = "https://microapigo.licindia.in/LICComMail/v1/"
         const val END_POINT_POKEMONS = "pokemon"
 
         const val DEFAULT_PAGE_NUMBER = "1"
